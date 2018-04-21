99d83cb360199191acab1a80f7597d4e830f028d
Hieu <normally96@gmail.com>
2018-04-21 11:38:02 +0700

    test TCP/Ip

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ server.on("connection", function(){
 	console.log("new connection is made");
 });
 
-server.listen(3000,function(){
+server.listen(process.env.PORT || 3000,function(){
 	console.log("server listen to 3000 port");
 });
